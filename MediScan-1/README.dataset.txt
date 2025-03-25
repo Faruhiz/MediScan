ef89303ff044690dea6e7e131bdd383701b09ed6
SusiShushii <thanaphatsumrith@gmail.com>
2025-03-26 06:47:25 +0700

    added deploy and predict

--- MediScan-1/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# MediScan > 2024-11-20 5:11am
-https://universe.roboflow.com/mediscan-1ggr5/mediscan-unpxf
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- test.py
+++
@@ -1,4 +0,0 @@
-if __name__ == "__main__":
-    from ultralytics import YOLO
-    model = YOLO("yolov8n-seg.pt")
-    model.train(data="C:/MediScan/seg_dataset/data.yaml", epochs=50, batch=16, imgsz=640)
